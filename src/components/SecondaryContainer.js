264ade7abec931eee1df38522b60693615471143
Hrishikesh Rajan <hrishikeshrajan3@gmail.com>
2023-10-16 22:28:10 +0530

    Dummy cards deleted

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -2,20 +2,15 @@ import React from 'react';
 import MovieList from './MovieList';
 import { useSelector } from 'react-redux';
 
-
 const SecondaryContainer = () => {
+  const movies = useSelector((store) => store.movies);
 
-  const movies = useSelector((store)=>store.movies)
-  
   return (
-    <div className='p-1 md:-top-40 inset-x-0  relative  z-40' >
-
-      <div >
-      < MovieList title='Now Playing' movies = {movies.nowPlayingMovies } />
-       < MovieList title='Popular' movies = {movies.popular } />
-       < MovieList title='Top Rated ' movies = {movies.topRated } />
-       < MovieList title='Fantasy' movies = {movies.nowPlayingMovies } />
-       < MovieList title='Scientific' movies = {movies.nowPlayingMovies } />
+    <div className="p-1 md:-top-40 inset-x-0  relative  z-40">
+      <div>
+        <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
+        <MovieList title="Popular" movies={movies.popular} />
+        <MovieList title="Top Rated " movies={movies.topRated} />
       </div>
     </div>
   );
