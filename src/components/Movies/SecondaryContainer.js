e5a00a6d7132edf34d414c80312c66652279ea9b
Hrishikesh Rajan <hrishikeshrajan3@gmail.com>
2023-10-19 20:22:35 +0530

    MovieList and MovieCard Component moved to Movies/MovieCollection dir

--- src/components/Movies/SecondaryContainer.js
+++ src/components/Movies/SecondaryContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import MovieList from './movie/MovieList';
+import MovieList from './MovieCollection/MovieList';
 import { useSelector } from 'react-redux';
 
 const SecondaryContainer = () => {
