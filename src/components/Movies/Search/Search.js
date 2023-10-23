e0ba24d396337310db1b4128e9c82edcf7a3d27d
Hrishikesh Rajan <hrishikeshrajan3@gmail.com>
2023-10-23 23:00:57 +0530

    Search bar dropdown overlap bug fixed

--- src/components/Movies/Search/Search.js
+++ src/components/Movies/Search/Search.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import MovieSuggestion from './MovieSuggestion';
-import { Background } from '../../../utils/constants';
 import { useSelector } from 'react-redux';
 
 const Search = () => {
   const movies = useSelector((store) => store.movies);
 
   return (
-    <div>
+    <div className='z-40'>
       <SearchBar />
       {movies.movieSuggestions && (
         <MovieSuggestion movies={movies.movieSuggestions?.results} />
