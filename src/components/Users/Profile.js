0be327b6d8262092e2aefd33cb616e66b75f9d28
Hrishikesh Rajan <hrishikeshrajan3@gmail.com>
2023-10-23 22:44:07 +0530

    Language option in setting tab added

--- src/components/Users/Profile.js
+++ src/components/Users/Profile.js
@@ -6,6 +6,7 @@ import {
   faAngleRight,
 } from '@fortawesome/free-solid-svg-icons';
 import useSignOut from '../../hooks/useSignout';
+import { SUPPORTED_LANGUAGES } from '../../utils/languageConstant';
 
 const Profile = () => {
   const [tab, setTab] = useState('profile');
@@ -13,13 +14,23 @@ const Profile = () => {
   const handleTabEvent = (e) => {
     setTab(e.target.getAttribute('data-tab'));
   };
+  const [lang, setLang] = useState({
+    code: SUPPORTED_LANGUAGES[0].code,
+    name: SUPPORTED_LANGUAGES[0].name,
+  });
+  const handleLanguage = (code, name) => {
+    setLang({ code, name });
+  };
+
+  const [lanuageOpt, setLanguageOpt] = useState(false);
+
   return (
     <div className="w-screen px-10 pt-20">
       <ul className="flex justify-start">
         <li
           className={`p-4 mx-2 ${
             tab === 'profile' && 'border-b-2'
-          } text-white cursor-pointer`}
+          } text-white text-xs cursor-pointer`}
           data-tab="profile"
           onClick={(e) => handleTabEvent(e)}
         >
@@ -28,11 +39,20 @@ const Profile = () => {
         <li
           className={`p-4 mx-2 ${
             tab === 'account' && 'border-b-2'
-          } text-white cursor-pointer`}
+          } text-white text-xs cursor-pointer`}
           data-tab="account"
           onClick={(e) => handleTabEvent(e)}
         >
-          Your Account
+          Account
+        </li>
+        <li
+          className={`p-4 mx-2 text-xs ${
+            tab === 'settings' && 'border-b-2'
+          } text-white cursor-pointer`}
+          data-tab="settings"
+          onClick={(e) => handleTabEvent(e)}
+        >
+          Settings
         </li>
       </ul>
 
@@ -72,6 +92,36 @@ const Profile = () => {
                   <FontAwesomeIcon icon={faRightFromBracket} /> Signout{' '}
                 </h4>
               </Link>
+            </div>
+          </div>
+        )}
+        {tab === 'settings' && (
+          <div className="flex flex-col" id="tab2">
+            <div onClick={() => setLanguageOpt(!lanuageOpt)} className="border-2 border-slate-300 p-7 my-2 rounded lg:w-4/12">
+              <div >
+                <h4 className="font-semibold text-xs text-white flex justify-between">
+                  <span> Preffered Language</span>
+                  <FontAwesomeIcon className={`${lanuageOpt?'transform rotate-90':''}`} icon={faAngleRight} />
+                </h4>
+              </div>
+
+              {lanuageOpt && (
+                <div className={`w-full mt-10 `} >
+                  <ul>
+                    {SUPPORTED_LANGUAGES.map((language, index) => (
+                      <li
+                        key={index}
+                        className="text-white py-3 mx-2 text-sm font-semibold "
+                        onClick={() =>
+                          handleLanguage(language.code, language.name)
+                        }
+                      >
+                        {language.name}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
             </div>
           </div>
         )}
